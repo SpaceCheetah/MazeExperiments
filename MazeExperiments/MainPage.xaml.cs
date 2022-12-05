7626a0a642c39fddeda510b072be705e72ef52de
SpaceCheetah <space-cheetah@hotmail.com>
2022-12-04 19:25:35 -0800

    Add click and drag to set start and goal

--- MazeExperiments/MainPage.xaml.cs
+++ MazeExperiments/MainPage.xaml.cs
@@ -1,24 +1,60 @@
-﻿namespace MazeExperiments;
+﻿using System.Diagnostics;
+
+namespace MazeExperiments;
 
 public partial class MainPage : ContentPage {
+    (int x, int y) _nodeStart = (0, 0);
+    (int x, int y) _nodeEnd = (19, 19);
+    RectangularMaze _maze;
+
 	public MainPage() {
 		InitializeComponent();
-        UpdateMaze(20, 20, 0, 0, 19, 19, 0, 0, true, new Random(0));
+        UpdateMaze(true);
 	}
 
-	void UpdateMaze(int width, int height, int startX, int startY, int endX, int endY, double jumpChance, double breakChance, bool showSolution, Random rand) {
-        var maze = new RectangularMaze(width, height);
-        MazeGenerator.GenerateMaze(rand, maze, (0, 0), jumpChance, breakChance);
-        List<(int, int)> solution = showSolution ? MazeSolver.GetBestPath((startX, startY), (endX, endY), maze) : null;
-        MazeView.Drawable = new RectangularMazeDrawable(maze, solution);
+    void NormalizeNode(ref (int x, int y) node, (int x, int y) max) {
+        if(node.x >= max.x) {
+            node.x = max.x - 1;
+        }
+        if (node.y >= max.y) {
+            node.y = max.y - 1;
+        }
+    }
+
+    void UpdateMaze(bool generationUpdated) {
+        if(generationUpdated) {
+            Random rand = new Random((int)SeedSlider.Value);
+            _maze = new RectangularMaze((int)WidthSlider.Value, (int)HeightSlider.Value);
+            MazeGenerator.GenerateMaze(rand, _maze, (0, 0), JumpChanceSlider.Value, BreakChanceSlider.Value);
+        }
+        //Recreate the random each time, as a maze with the same paramaters should be the same
+        NormalizeNode(ref _nodeStart, (_maze.Width, _maze.Height));
+        NormalizeNode(ref _nodeEnd, (_maze.Width, _maze.Height));
+        
+        List<(int, int)> solution = ShowSolution.IsChecked ? MazeSolver.GetBestPath(_nodeStart, _nodeEnd, _maze) : null;
+        MazeView.Drawable = new RectangularMazeDrawable(_maze, _nodeStart, _nodeEnd, solution);
     }
 
     void OnValueChanged(object sender, EventArgs e) {
-        UpdateMaze((int)WidthSlider.Value, (int)HeightSlider.Value, 0, 0, (int)WidthSlider.Value - 1, (int)HeightSlider.Value - 1, JumpChanceSlider.Value, BreakChanceSlider.Value, ShowSolution.IsChecked, new Random((int)SeedSlider.Value));
+        UpdateMaze(true);
     }
 
-    private void CheckChanged(object sender, CheckedChangedEventArgs e) {
-        OnValueChanged(null, null);
+    void CheckChanged(object sender, CheckedChangedEventArgs e) {
+        UpdateMaze(false);
+    }
+
+    void MazeView_StartInteraction(object sender, TouchEventArgs e) {
+        _nodeStart = ((RectangularMazeDrawable)MazeView.Drawable).HitTest(e.Touches[0].X, e.Touches[0].Y);
+        _nodeEnd = _nodeStart;
+        UpdateMaze(false);
+    }
+
+    void MazeView_DragInteraction(object sender, TouchEventArgs e) {
+        (int x, int y) = ((RectangularMazeDrawable)MazeView.Drawable).HitTest(e.Touches[0].X, e.Touches[0].Y);
+        if (_nodeEnd == (x, y)) return;
+        if ((x, y) == (-1, -1)) return; //last drawable hasn't been used yet, so hit test fails
+        _nodeEnd = (x, y);
+        UpdateMaze(false);
     }
 }
 
--- MazeExperiments/RectangularMazeDrawable.cs
+++ MazeExperiments/RectangularMazeDrawable.cs
@@ -2,16 +2,33 @@
 internal class RectangularMazeDrawable : IDrawable {
     readonly RectangularMaze _maze;
     readonly List<(int x, int y)> _solution;
-    public RectangularMazeDrawable(RectangularMaze maze, List<(int x, int y)> solution = null) {
+    float _cellWidth = -1;
+    float _cellHeight = -1;
+    (int x, int y) _start, _end;
+
+    public RectangularMazeDrawable(RectangularMaze maze, (int x, int y) start, (int x, int y) end, List<(int x, int y)> solution = null) {
         _maze = maze;
         _solution = solution;
+        _start = start;
+        _end = end;
+    }
+
+    public (int x, int y) HitTest(float x, float y) {
+        if (_cellWidth <= 0 || _cellHeight <= 0) return (-1, -1);
+        return ((int)(x / _cellWidth),(int)( y / _cellHeight));
     }
 
     public void Draw(ICanvas canvas, RectF dirtyRect) {
         if (_maze.Width == 0 || _maze.Height == 0) return;
-        float cellWidth = dirtyRect.Width / _maze.Width;
-        float cellHeight = dirtyRect.Height / _maze.Height;
-        canvas.StrokeSize = Math.Min(cellWidth, cellHeight) / 10;
+        _cellWidth = dirtyRect.Width / _maze.Width;
+        _cellHeight = dirtyRect.Height / _maze.Height;
+
+        canvas.FillColor = Colors.Blue;
+        canvas.FillRectangle(_start.x * _cellWidth, _start.y * _cellHeight, _cellWidth, _cellHeight);
+        canvas.FillColor = Colors.Gold;
+        canvas.FillRectangle(_end.x * _cellWidth, _end.y * _cellHeight, _cellWidth, _cellHeight);
+
+        canvas.StrokeSize = Math.Min(_cellWidth, _cellHeight) / 10;
         canvas.StrokeColor = Application.Current.RequestedTheme == AppTheme.Dark ? Colors.White : Colors.Black;
         canvas.StrokeLineCap = LineCap.Round;
         canvas.DrawRectangle(dirtyRect);
@@ -19,10 +36,10 @@ internal class RectangularMazeDrawable : IDrawable {
             for(int x = 0; x < _maze.Width; x++) {
                 RectangularMaze.CellPassages passages = _maze.GetCellPassages(x, y);
                 if(!passages.Right && x != _maze.Width - 1) {
-                    canvas.DrawLine(dirtyRect.Left + cellWidth * x + cellWidth, dirtyRect.Top + cellHeight * y, dirtyRect.Left + cellWidth * x + cellWidth, dirtyRect.Top + cellHeight * y + cellHeight);
+                    canvas.DrawLine(dirtyRect.Left + _cellWidth * x + _cellWidth, dirtyRect.Top + _cellHeight * y, dirtyRect.Left + _cellWidth * x + _cellWidth, dirtyRect.Top + _cellHeight * y + _cellHeight);
                 }
                 if(!passages.Down && y != _maze.Height - 1) {
-                    canvas.DrawLine(dirtyRect.Left + cellWidth * x, dirtyRect.Top + cellHeight * y + cellHeight, dirtyRect.Left + cellWidth * x + cellWidth, dirtyRect.Top + cellHeight * y + cellHeight);
+                    canvas.DrawLine(dirtyRect.Left + _cellWidth * x, dirtyRect.Top + _cellHeight * y + _cellHeight, dirtyRect.Left + _cellWidth * x + _cellWidth, dirtyRect.Top + _cellHeight * y + _cellHeight);
                 }
             }
         }
@@ -30,7 +47,7 @@ internal class RectangularMazeDrawable : IDrawable {
         canvas.StrokeColor = Colors.Green;
         (int lastX, int lastY) = _solution.First();
         foreach ((int x, int y) in _solution) {
-            canvas.DrawLine(dirtyRect.Left + cellWidth * lastX + cellWidth / 2, dirtyRect.Top + cellHeight * lastY + cellHeight / 2, dirtyRect.Left + cellWidth * x + cellWidth / 2, dirtyRect.Top + cellHeight * y + cellHeight / 2);
+            canvas.DrawLine(dirtyRect.Left + _cellWidth * lastX + _cellWidth / 2, dirtyRect.Top + _cellHeight * lastY + _cellHeight / 2, dirtyRect.Left + _cellWidth * x + _cellWidth / 2, dirtyRect.Top + _cellHeight * y + _cellHeight / 2);
             (lastX, lastY) = (x, y);
         }
     }
